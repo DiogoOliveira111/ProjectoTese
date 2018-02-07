8d2be437d5cb05b8c5db98c667a88bbb636fb538
DiogoOliveira111 <diogooliveira111@hotmail.com>
2018-02-07 10:31:20 +0000

    firsttry

---
+++ OpenFiles.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pickle
+import seaborn as sns
+import numpy as np
+import easygui
+
+path = easygui.fileopenbox()
+
+with open(path, 'rb') as handle:
+    collection= pickle.load(handle)
