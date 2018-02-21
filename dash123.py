93625585cb803d964db3e366954c34d1f42e5255
DiogoOliveira111 <diogooliveira111@hotmail.com>
2018-02-21 17:43:47 +0000

    DASH 123 O radio button esta a funcionar mas a primeira vez que corre da sempre erro porque a variavel selected_option é uma lista e nao esta a converter para string---- FIXED Quero fazer um button que quando carregado altera o grafico da pos x e y para a interpolaçao e escreve as space_vars max e min. Estou a ter problemas em fazer com que o click no botao o faça- FIXED
    Falta formatar as variaveis de espaco decentemente e tirar o bug que nao permite trocar entre o heatmap e o positional map.- Fixed, formataçao por arranjar
    
    Falta por o parser dos filtros a funcionar, a variavel data nao pode entrar como parametro de entrada para os filtros pq tem montes de cenas para alem dos dados a filtrar.
    Bug de so aparecer a string no parser depois de clicar no interpolate

--- dash123.py
+++ dash123.py
@@ -1,3 +1,4 @@
+from dash.dependencies import Input, Output, State, Event
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,11 +10,99 @@ from tkinter import Tk, Label
 from WBMTools.sandbox.interpolation import interpolate_data
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
 traces =[]
+prevb1=0
+prevb2=0
+prevb3=0
+finalStr=""
+from scipy import signal
+from scipy.signal import filtfilt
+
+#Extra Functions
+
+def lowpass(s, f, order=2, fs=1000.0, use_filtfilt=True):
+    """
+    @brief: for a given signal s rejects (attenuates) the frequencies higher
+    then the cuttof frequency f and passes the frequencies lower than that
+    value by applying a Butterworth digital filter
+    @params:
+    s: array-like
+    signal
+    f: int
+    the cutoff frequency
+    order: int
+    Butterworth filter order
+    fs: float
+    sampling frequency
+    @return:
+    signal: array-like
+    filtered signal
+    """
+    b, a = signal.butter(order, f / (fs / 2))
+
+    if use_filtfilt:
+        return filtfilt(b, a, s)
+
+    return signal.lfilter(b, a, s)
+
+def highpass(s, f, order=2, fs=1000.0, use_filtfilt=True):
+    """
+    @brief: for a given signal s rejects (attenuates) the frequencies lower
+    then the cuttof frequency f and passes the frequencies higher than that
+    value by applying a Butterworth digital filter
+    @params:
+    s: array-like
+    signal
+    f: int
+    the cutoff frequency
+    order: int
+    Butterworth filter order
+    fs: float
+    sampling frequency
+    @return:
+    signal: array-like
+    filtered signal
+    """
+
+    b, a = signal.butter(order, f * 2 / (fs / 2), btype='highpass')
+    if use_filtfilt:
+        return filtfilt(b, a, s)
+
+    return signal.lfilter(b, a, s)
+
+def bandpass(s, f1, f2, order=2, fs=1000.0, use_filtfilt=True):
+    """
+    @brief: for a given signal s passes the frequencies within a certain range
+    (between f1 and f2) and rejects (attenuates) the frequencies outside that
+    range by applying a Butterworth digital filter
+    @params:
+    s: array-like
+    signal
+    f1: int
+    the lower cutoff frequency
+    f2: int
+    the upper cutoff frequency
+    order: int
+    Butterworth filter order
+    fs: float
+    sampling frequency
+    @return:
+    signal: array-like
+    filtered signal
+    """
+    b, a = signal.butter(order, [f1 * 2 / fs, f2 * 2 / fs], btype='bandpass')
+
+    if use_filtfilt:
+        return filtfilt(b, a, s)
+
+    return signal.lfilter(b, a, s)
+
 path = easygui.fileopenbox()
 
 with open(path, 'rb') as handle:
     collection= pickle.load(handle)
+
 
 flag=0
 MouseTime=[]
@@ -126,8 +215,84 @@ html.Div(
     html.Div(id='output_spacevar'),
              dcc.Markdown(id='text_spacevar')
 
-])])
+]),
+    html.Div([
+        html.Button('HighPass(H)', id='highpass', value=''),
+        html.Button('LowPass(L)', id='lowpass'),
+        html.Button('BandPass(Bp)', id='bandpass')]
+),
+    html.Div(dcc.Input(id='PreProcessing',
+    placeholder='Enter a value...',
+    type='text',
+    value=''
+)),
+    html.Div([
+        html.Button('Pre-Processing', id='preprocess'),
+        dcc.Graph(id='timevar_graph_PP')
+    ])
 
+])
+
+@app.callback(
+    dash.dependencies.Output('timevar_graph_PP', 'figure'),
+    [dash.dependencies.Input('preprocess', 'n_clicks'),
+    dash.dependencies.Input('PreProcessing', 'value'),
+    dash.dependencies.Input('timevar_graph', 'figure')]
+)
+
+def PreProcessStringParser(n_clicks, parse, data):
+    if(n_clicks!=None):
+        CutString=parse.split()
+        for i in range(len(CutString)-1):
+            if(CutString[i] =='H'):
+                #function high pass
+                print(data)
+                data=highpass(data,int(CutString[i+1]))
+                print('asasasas')
+                # pass
+            elif(CutString[i] =='L'):
+                #Function Low Pass
+                print(2)
+                # pass
+            elif (CutString[i] == 'BD'):
+                #Function Band Pass
+                print(3)
+                # pass
+    return data
+
+@app.callback(
+    dash.dependencies.Output('PreProcessing', 'value'),[
+    dash.dependencies.Input('highpass', 'n_clicks'),
+    dash.dependencies.Input('lowpass', 'n_clicks'),
+    dash.dependencies.Input('bandpass', 'n_clicks')],
+    [dash.dependencies.State('PreProcessing', 'value')]
+    # prev_inputs=[
+    #         dash.dependencies.PrevInput('button-1', 'n_clicks'),
+    #         dash.dependencies.PrevInput('button-2', 'n_clicks'),
+    #         dash.dependencies.PrevInput('button-3', 'n_clicks'),
+    #         ]
+)
+
+
+
+def PreProcessingWrite(b1,b2,b3, finalStr):
+    global  prevb1, prevb2, prevb3 #banhada com variaveis globais para funcionar, convem mudar
+    if(b1!= None): # tem o problema de nao limpar, se calhar precisa de um botao para limpar
+        if(b1>prevb1):
+            finalStr+= 'H '
+        prevb1 = b1
+
+    if (b2 != None):
+        if (b2 >prevb2):
+            finalStr += 'L '
+        prevb2 = b2
+
+    if (b3 != None):
+        if (b3 >prevb3):
+            finalStr +="BD "
+        prevb3 = b3
+
+    return finalStr
 
 
 @app.callback(
@@ -135,12 +300,26 @@ html.Div(
     [dash.dependencies.Input('Radio', 'value')])
 def update_figure(selected_option):
 
-    #print(space_var)
+    #print(len(time_var['jerk']))
     #print(len(time_var['xt']))
     #filtered_df = dM[selected_option]
-
-    traces=[]
-    traces.append(go.Scatter(
+    if(str(selected_option) in ("vt, vx, vy")):
+        traces=[]
+        traces.append(go.Scatter(
+                x=time_var['ttv'],
+                y=time_var[str(selected_option)],
+                text=selected_option[0],
+                opacity=0.7,
+                # marker={
+                #     'size': 5,
+                #     'line': {'width': 0.5, 'color': 'white'}
+                # },
+                line= {'width': 2, 'color': 'black'},
+                name=i
+            ))
+    elif(str(selected_option) in ("xt, yt, a, jerk")):
+        traces = []
+        traces.append(go.Scatter(
             x=time_var['tt'],
             y=time_var[str(selected_option)],
             text=selected_option[0],
@@ -149,11 +328,9 @@ def update_figure(selected_option):
             #     'size': 5,
             #     'line': {'width': 0.5, 'color': 'white'}
             # },
-            line= {'width': 2, 'color': 'black'},
+            line={'width': 2, 'color': 'black'},
             name=i
         ))
-
-
     return {
         'data': traces,
         'layout': go.Layout(
@@ -170,9 +347,9 @@ def update_figure(selected_option):
      dash.dependencies.Input('Radioheatmap', 'value')])
 def interpolate_graf(n_clicks, value):
     if(value=='xy'):
-        if(n_clicks == None):
-            pass
-        else:
+        # if(n_clicks == None):
+        #     pass
+        if (n_clicks!=None): #1ª vez n_clicks== None
             traces = []
             traces.append(go.Scatter(
                 x=space_var['xs'],
@@ -197,16 +374,61 @@ def interpolate_graf(n_clicks, value):
                 )
             }
     elif(value =='heat'):
-        x=MouseDict['x']
-        y=MouseDict['y']
-        traces=[go.Histogram2d(x=x, y=y,
-                            #colorscale='YIGnBu',
-                            zmax=10,
-                            nbinsx=30,
-                            nbinsy=30,
-                            zauto=False)]
+        x=space_var['xs']
+        y=space_var['ys']
+        colorscale = ['#7A4579', '#D56073', 'rgb(236,158,105)', (1, 1, 0.2), (0.98, 0.98, 0.98)]
+        # traces=[
+
+        trace1 = go.Scatter(
+            x=x, y=y, mode='markers', name='points',
+            marker=dict(color='rgb(102,0,0)', size=5, opacity=0.4)
+        )
+        trace2 = go.Histogram2dcontour(
+            x=x, y=y, name='density', ncontours=20,
+            colorscale='Hot', reversescale=True, showscale=False
+        )
+
+        data = [trace1, trace2]
+
+        layout = go.Layout(
+            showlegend=False,
+            autosize=True,
+            # width=600,
+            # height=550,
+            xaxis=dict(
+                domain=[0, 0.85],
+                showgrid=False,
+                zeroline=False
+            ),
+            yaxis=dict(
+                domain=[0, 0.85],
+                showgrid=False,
+                zeroline=False
+            ),
+            margin=dict(
+                t=50
+            ),
+            hovermode='closest',
+            bargap=0,
+            xaxis2=dict(
+                domain=[0.85, 1],
+                showgrid=False,
+                zeroline=False
+            ),
+            yaxis2=dict(
+                domain=[0.85, 1],
+                showgrid=False,
+                zeroline=False
+            )
+        )
+            # go.Histogram2d(x=x, y=y,
+            #                 #colorscale='YIGnBu',
+            #                 zmax=10,
+            #                 nbinsx=50,
+            #                 nbinsy=50,
+            #                 zauto=False)]
         return {
-            'data': traces,
+            'data': data,
             'layout': go.Layout(
                 xaxis=dict(ticks='', showgrid=False, zeroline=False, nticks=20),
                 yaxis=dict(ticks='', showgrid=False, zeroline=False, nticks=20),
@@ -220,8 +442,8 @@ def interpolate_graf(n_clicks, value):
               [dash.dependencies.Input('interpolate', 'n_clicks')])
 def display_spacevar(n_clicks):
     if(n_clicks!= None):
-        return "Length of strokes: [{0}, {1}] /n" \
-                "Straightness: [{0}, {1}] /n " \
+        return "Length of strokes: [{0}, {1}] px/items/n" \
+                "Straightness: [{0}, {1}] px/px/n " \
                "Jitter: [{4}]  /n" \
                 "Angles: [{5}, {6}] /n" \
                "Angular Velocity (w): [{7}, {8}] /n"\
@@ -264,3 +486,4 @@ def display_spacevar(n_clicks):
 
 if __name__ == '__main__':
     app.run_server()
+
